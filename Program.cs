140d487ef3cf58751260c3e3fe174bc50da49fe3
mohamedderouiche <mohamedamine.derouiche@esprit.tn>
2024-12-27 15:27:13 +0100

    update

--- Program.cs
+++ Program.cs
@@ -29,47 +29,50 @@
 
 
 
-
 using GymApplication.Repository.Models;
 using GymApplication.Repository;
 using Microsoft.EntityFrameworkCore;
 using GymApplication.UtilityService;
 using Microsoft.Extensions.FileProviders;
 using Stripe;
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddCors((options) =>
+// Add CORS policy
+builder.Services.AddCors(options =>
 {
-    options.AddPolicy("angularApplication", (builder) =>
+    options.AddPolicy("angularApplication", policy =>
     {
-        builder.WithOrigins("http://localhost:4200")
-        .AllowAnyHeader()
-        .AllowAnyMethod(); 
+        policy.WithOrigins("http://localhost:4200") // Add your frontend's production URL here if needed
+              .AllowAnyHeader()
+              .AllowAnyMethod();
     });
 });
 
-// Add services to the container.
+// Add services to the container
 builder.Services.AddDbContext<GymDbContext>(options =>
     options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
     ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))));
 
-
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<IEmailService, EmailService>();
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-StripeConfiguration.ApiKey = app.Configuration.GetSection("Stripe:SecretKey").Get<String>();
-// Configure the HTTP request pipeline.
+StripeConfiguration.ApiKey = app.Configuration.GetSection("Stripe:SecretKey").Get<string>();
+
+// Apply CORS policy for all environments
+app.UseCors("angularApplication");
+
+// Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
-    app.UseCors("angularApplication");
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
@@ -79,4 +82,3 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
-
